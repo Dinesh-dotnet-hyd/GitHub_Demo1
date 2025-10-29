dcb6b97468cdc86e8c576f96475aa863efa96fd5
Lokesh Gunaganti <Lokesh.Gunaganti@encora.com>
2025-10-29 17:30:16 +0530

    Updated Student Class

--- GitHub_Demo1/Student.cs
+++ GitHub_Demo1/Student.cs
@@ -9,6 +9,7 @@ namespace GitHub_Demo1
     internal class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }   
+        public string Description { get; set; }
     }
 }
